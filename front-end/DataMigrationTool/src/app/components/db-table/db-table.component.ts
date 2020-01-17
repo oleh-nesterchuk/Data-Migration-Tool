1ff98648804da648ec9105c798f3c880f4f03136
Oleh Nesterchuk <olegnesterchuk@gmail.com>
2020-01-17 14:33:05 +0200

    Cleared up db-table component adding @Input properties

--- front-end/DataMigrationTool/src/app/components/db-table/db-table.component.ts
+++ front-end/DataMigrationTool/src/app/components/db-table/db-table.component.ts
@@ -16,17 +16,15 @@ export class DbTableComponent implements OnInit {
   columns = ['ID', 'Name', 'Surname', 'Birth Date', 'Age', 'Emails', 'Transfer'];
   columnNames: ['ID', 'firstName', 'lastName', 'birthDate', 'age', 'emails'];
   isSql: boolean;
-  transferString: string;
-  emailString: string;
-  userString: string;
-  neighbourTable: string;
+  @Input() transferString: string;
+  @Input() emailString: string;
+  @Input() userString: string;
+  @Input() neighbourTable: string;
   @Input() tableName: string;
   @Input() tableHeader: string;
 
-  constructor(private httpService: UserService,
-              protected data: DataService,
-              private modalService: NgbModal,
-              private dragulaService: DragulaService) {
+  constructor(private httpService: UserService, protected data: DataService,
+              private modalService: NgbModal, private dragulaService: DragulaService) {
                 const group = this.dragulaService.find('COPYABLE');
                 if (group === undefined) {
                   this.dragulaService.createGroup('COPYABLE', {
@@ -36,20 +34,6 @@ export class DbTableComponent implements OnInit {
               }
 
   ngOnInit() {
-    if (this.tableHeader.includes('SQL')) {
-      this.isSql = true;
-      this.transferString = 'SendToMongoDb';
-      this.emailString = 'SqlServerEmail';
-      this.userString = 'SqlServerUser';
-      this.neighbourTable = 'mongoUsers';
-    } else {
-      this.isSql = false;
-      this.transferString = 'SendToSqlServer';
-      this.emailString = 'MongoDbEmail';
-      this.userString = 'MongoDbUser';
-      this.neighbourTable = 'sqlUsers';
-    }
-
     this.loadUsers();
   }
 
